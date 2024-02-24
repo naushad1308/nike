f2fbabbd747ec8a56861208d5890fb1fc0f79b29
naushad1308 <naushad1308@gmail.com>
2024-02-24 19:07:17 +0530

    nike updated

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Button from "./Button";
+import PopularProductCard from "./PopularProductCard";
+import ShoeCard from "./ShoeCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+
+export {
+    Button,
+    ShoeCard,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard
+}
